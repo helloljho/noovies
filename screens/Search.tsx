807d0e32caef25a569f97c80873044296cfd5b40
helloljho <helloljho@gmail.com>
2022-01-18 11:54:47 +0900

    fix : Search Input Component

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import styled from 'styled-components/native';
 
-const Search = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Search</Text>
-  </View>
-);
+const Container = styled.ScrollView``;
+const SearchBar = styled.TextInput`
+  background-color: white;
+  padding: 10px 15px;
+  border-radius: 15px;
+  width: 90%;
+  margin: 10px auto;
+`;
+
+const Search = () => {
+  const [query, setQuery] = useState('');
+  const onChangeTest = (text: string) => {
+    setQuery(text);
+  };
+  return (
+    <Container>
+      <SearchBar
+        placeholder="Serach for Movie or TV Show"
+        placeholderTextColor="grey"
+        returnKeyType="search"
+        onChangeText={onChangeTest}
+      />
+    </Container>
+  );
+};
 
 export default Search;
